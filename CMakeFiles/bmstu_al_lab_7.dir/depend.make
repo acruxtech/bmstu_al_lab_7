8df1828f95833cd5fdd8b63540a17c55392bc06e
acruxtech <acrux_tech@mail.ru>
2024-11-29 21:47:04 +0300

    delete build files

--- CMakeFiles/bmstu_al_lab_7.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bmstu_al_lab_7.
-# This may be replaced when dependencies are built.
--- source/task1.cpp
+++ source/task1.cpp
@@ -6,7 +6,7 @@
 
 void saveToFile(const std::string& filename, const std::vector<std::string>& data) {
     std::fstream file;
-    file.open("assets/" + filename, std::ios::out);
+    file.open("../assets/" + filename, std::ios::out);
     if (file.is_open()) {
         for (std::string word : data) {
             file << word << " ";
@@ -21,7 +21,7 @@ void saveToFile(const std::string& filename, const std::vector<std::string>& dat
 
 void loadFromFile(const std::string& filename, std::vector<std::string>& outData) {    
     std::fstream file;
-    file.open("assets/" + filename, std::ios::in);
+    file.open("../assets/" + filename, std::ios::in);
     if (file.is_open()) {
         std::string word;
         while (file >> word) {
